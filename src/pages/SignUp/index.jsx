5e33960c56b275d17906f173e0bcae94e19feb29
Laignier <michellelaignier17@gmail.com>
2023-09-17 12:49:35 -0300

    sign up page

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,27 @@
+import { FiMail, FiLock, FiUser} from 'react-icons/fi'
+
+import { Container, Form, Img } from './styles'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+export function SignUp() {
+  return(
+    <Container>
+      <Img/>
+
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder="Nome" type="text" required icon={FiUser}/>
+        <Input placeholder="E-mail" type="email" required icon={FiMail}/>
+        <Input placeholder="Senha" type="password" required icon={FiLock}/>
+
+        <Button title="Cadastrar" />
+				<a href="#"> Voltar para o login</a>
+      </Form>
+    </Container>
+  )
+}
