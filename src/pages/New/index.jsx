dc7426b35d6db2a21d6fbdf903d769d1c1033834
Laignier <michellelaignier17@gmail.com>
2023-10-12 19:06:50 -0300

    new note: add links

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Container, Form } from './styles'
@@ -10,6 +11,23 @@ import { Section } from '../../components/Section'
 import { Button } from '../../components/Button'
 
 export function New() {
+	// Pra guardar todos os links:
+	const [links, setLinks] = useState([])
+	// Pra guardar o novo link que vai ser adicionado:
+	const [newLink, setNewLink] = useState("")
+
+	function handleAddLink() {
+		if(!newLink) {
+			return
+		}
+
+		// Pode ser qualquer nome no lugar de prevState
+		setLinks(prevState => [...prevState, newLink]) // imutabilidade
+		// setLinks(newLink) assim vai ficar só o link mais recente
+
+		setNewLink("") // Pra ter o estado "resetado" de novo
+	}
+
   return(
     <Container>
       <Header/>
@@ -25,8 +43,16 @@ export function New() {
 					<TextArea placeholder="Observações"/>
 
 					<Section title="Links úteis">
-						<NoteItem value="https://rocketseat.com.br"/>
-						<NoteItem isNew placeholder="Novo link"/>
+						{
+							links.map((link, index) => ( // index é a posição do elemento na lista
+								<NoteItem
+								  key={String(index)} // Sempre que tem um componente renderizado por uma lista, precisa da propriedade key
+									value={link} // Pegar o valor de cada link
+									onClick={() => {}}
+								/>
+							))
+						}
+						<NoteItem isNew placeholder="Novo link" value={newLink} onChange={e => setNewLink(e.target.value)} onClick={handleAddLink}/>
 					</Section>
 
 					<Section title="Marcadores" className='marcadores'>
